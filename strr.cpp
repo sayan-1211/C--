7827cc2ae02512c659adb1f08c1c319b125d1659
ronnie47 <sayanlaha47@gmail.com>
2023-07-31 14:15:29 +0530

    first commit

---
+++ strr.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int l1,l2;
+    string str1,str2="ronnie";
+    cout << "Enter a name"<< endl;
+    cin >> str1;
+    l1=str1.size();
+    l2=str2.size();
+    cout << l2 << endl;
+    return 0;
+}
